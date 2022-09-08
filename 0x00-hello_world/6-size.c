ed89408f0f9dc88d2413f6c7bbc749c243af475e
don91204 <don91204@gmail.com>
2022-09-08 09:10:49 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
 * main - print the string in the main function
-* 
+*
 * Description: using the main function
 * with proper grammar, but the outcome is a piece of art
 * Return: 0
@@ -14,11 +14,11 @@ int i;
 long li;
 long long lli;
 float f;
-  
-printf("size of a char: %ld bytes(s)\n", sizeof(c));
-printf("size of an int: %ld byte(s)\n", sizeof(i));
-printf("size of a long int: %ld byte(s)\n", sizeof(li));
-printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
-printf("size of a float: %ld byte(s)\n", sizeof(f));
-return(0);
+
+printf("Size of a char: %ld bytes(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
+return (0);
 }
