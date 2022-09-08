62bc9b9826939568e216f16e71f4f48e66dcb380
don91204 <don91204@gmail.com>
2022-09-08 08:47:06 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /**
 * main - print the string in the put function
+*
+* Description: using the main function
+* with proper grammar, but the outcome is a piece of art
+* Return: 0
 */
 int main(void)
 {
