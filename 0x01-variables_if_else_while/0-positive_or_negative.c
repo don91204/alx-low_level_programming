e2638a9e56243dea100b4c66396d6e6c87250689
don91204 <don91204@gmail.com>
2022-09-09 08:26:51 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+/**
+*main - print the string in the put function
+*
+*Description: using the main function
+*Return: 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));  
+n = rand() - RAND_MAX / 2; 
+/**
+*your code goes there 
+*/
+if (n > 0) {
+printf ("is positive");
+}else if (n = 0){    
+printf ("is zero");
+}else{
+printf ("is negative"); 
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this is a 0x01-variables_if_else_while readme
