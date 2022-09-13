92d5f1225a234551a1e4424bb1a7a1cf8d91f73d
don91204 <don91204@gmail.com>
2022-09-13 06:44:30 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
 *
 * Return: Always 0.
 */
-int main()
+int main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -16,6 +16,6 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 
-return(0);
+return (0);
 
 }
