e7344eb0acadbd22b09bc190b016c91ea0248b5d
don91204 <don91204@gmail.com>
2022-09-22 07:32:26 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,34 +1,29 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
+ * leet -> a leet function
  *
- * @s: input string.
+ * @x: param x
  *
- * Return: the pointer to dest.
+ * Return: a string
  */
-
-char *cap_string(char *s)
+char *leet(char *x)
 {
-	int count = 0, i;
+	int a = 0, b, l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
 
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	while (x[a])
 	{
-		for (i = 0; i < 13; i++)
+		b = 0;
+
+		while (b < l)
 		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
+				x[a] = trw[b];
+			b++;
 		}
-		count++;
+		a++;
 	}
-	return (s);
+	return (x);
 }
