327668a2a41a71c6e5c1ba476622dd3d5ad66e87
don91204 <don91204@gmail.com>
2022-09-22 07:18:19 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,29 @@
 #include "main.h"
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
+ * cap_string -> capitalization function
  *
- * @s: input string.
+ * @x: string param.
  *
- * Return: the pointer to dest.
+ * Return: capitalized version of the string.
  */
 
-
-
-char *string_toupper(char *s)
-
+char *cap_string(char *x)
 {
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}'};
+	int len = 13;
+	int a = 0, i;
 
-	int count = 0;
-
-
-
-	while (*(s + count) != '\0')
-
+	while (x[a])
 	{
-
-		if ((*(s + count) >= 97) && (*(s + count) <= 122))
-
-			*(s + count) = *(s + count) - 32;
-
-		count++;
-
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				x[a] = x[a] - 32;
+			i++;
+		}
+		a++;
 	}
-
-
-
-	return (s);
-
+	return (x);
 }
